9b8f6237d6f503cc6f6ce7df6f08356f8c5e4c7a
BasheerAhmed141 <basheerm2006@gmail.com>
2025-06-26 18:58:04 +0530

    bash.html

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        
+
+        String  s1 = "BASHEER";
+        s1 = s1 +" Ahmed";
+        System.out.println(" "+s1);
+    }
+}
